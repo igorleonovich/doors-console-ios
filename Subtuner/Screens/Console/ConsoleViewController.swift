b4552749cdd9bf85c9a64845db9f3090f0642b07
name <email>
2020-05-10 10:29:32 +0300

    Console: Add Menu with Sign Out action

--- Subtuner/Screens/Console/ConsoleViewController.swift
+++ Subtuner/Screens/Console/ConsoleViewController.swift
@@ -10,5 +10,23 @@ import UIKit
 
 class ConsoleViewController: BaseViewController {
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     
+    @IBAction func menuButtonTapped(_ sender: Any) {
+        let optionMenu = UIAlertController(title: nil, message: "Menu", preferredStyle: .actionSheet)
+        let logoutAction = UIAlertAction(title: "Sign Out", style: .destructive, handler: { action in
+            
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        [logoutAction, cancelAction].forEach { optionMenu.addAction($0) }
+        self.parent?.present(optionMenu, animated: true, completion: nil)
+    }
 }
