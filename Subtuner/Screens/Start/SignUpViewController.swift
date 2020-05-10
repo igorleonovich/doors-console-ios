d87acd486e15e4d16ec40e3f7938f9d79b17d02b
name <email>
2020-05-10 08:56:06 +0300

    SignUp: Add username field

--- Subtuner/Screens/Start/SignUpViewController.swift
+++ Subtuner/Screens/Start/SignUpViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class SignUpViewController: BaseViewController {
     
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var confirmPasswordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
